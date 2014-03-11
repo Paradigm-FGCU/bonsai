6125858be84b7654feece9ab4d0fd37d90804c1b
Sergio Pais <sp@srge.org>
2014-03-11 13:22:25 -0400

    splash screen timer, ui icon changes

--- Bonsai/src/org/srge/bonsai/Activity_Main.java
+++ Bonsai/src/org/srge/bonsai/Activity_Main.java
@@ -3,25 +3,41 @@ package org.srge.bonsai;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+
 
 public class Activity_Main extends Activity {
+
+	// Splash screen timer
+	// http://www.androidhive.info/2013/07/how-to-implement-android-splash-screen-2/
+	final static int SPLASH_TIME_OUT = 1000;
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_start_screen);
 
-        setContentView(R.layout.activity_main_start_screen);
+		new Handler().postDelayed(new Runnable() {
 
-        final Button button_start = (Button) findViewById(R.id.button_start);
-        button_start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	Intent intent = new Intent(v.getContext(),Activity_Main_Menu.class);
-            	startActivityForResult(intent,0);
-            }
-        });
-        
-       
-        
-        
-    }
+			/*
+			 * Showing splash screen with a timer. This will be useful when you
+			 * want to show case your app logo / company
+			 */
+
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+				// Start your app main activity
+				Intent i = new Intent(Activity_Main.this, Activity_Main_Menu.class);
+				startActivity(i);
+
+				// close this activity
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+
+
+
 }
+
