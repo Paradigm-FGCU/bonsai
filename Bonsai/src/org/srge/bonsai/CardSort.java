0b1f8fa4358ebb0f3c06a4ae4ad1367632d514a3
Gbkuhn <kuhn.geoff@gmail.com>
2014-04-05 15:26:35 -0400

    sorting code

---
+++ Bonsai/src/org/srge/bonsai/CardSort.java
@@ -0,0 +1,38 @@
+package org.srge.bonsai;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+//on click listener in CardSectionFragment adds current card to incorrect list
+//then sorts into priority queue based on how many times it was incorrect
+//return newly made deck
+public class CardSort {
+	private static ArrayList<CardContent> incorrect_list;
+	static PriorityQueue<Object> pq = new PriorityQueue<Object>();
+	
+    Comparator<Object> comparator = new CardComparator();
+    ArrayList<CardContent> cardList = RunningInfo.getSelectedDeck().getCardList();
+    PriorityQueue<Object> queue = new PriorityQueue<Object>(cardList.size(), comparator);
+
+	
+	public static ArrayList<CardContent> add_incorrect_list(int card_number) {
+		RunningInfo.getWorkingCardList().get(card_number);
+		incorrect_list.add(RunningInfo.getWorkingCardList().get(card_number));	
+		pq.offer(RunningInfo.getWorkingCardList().get(card_number));
+		return incorrect_list;
+	}
+	
+	public class CardComparator implements Comparator<Object>
+	{	  
+
+		@Override
+		public int compare(Object left, Object right) {
+			//compare elements
+			return 0;
+		}
+	}
+
+
+}
