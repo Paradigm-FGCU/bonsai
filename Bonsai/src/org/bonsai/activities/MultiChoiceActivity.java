e8f55e962f6ca9bce7256b0743d517172aefd8e2
disotell <disotell@github.com>
2014-04-16 17:32:10 -0400

    Got Multi to works correctly

--- Bonsai/src/org/bonsai/activities/MultiChoiceActivity.java
+++ Bonsai/src/org/bonsai/activities/MultiChoiceActivity.java
@@ -1,7 +1,8 @@
 package org.bonsai.activities;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.Random;
 
 import org.bonsai.util.CActionBarActivity;
 import org.srge.bonsai.R;
@@ -9,9 +10,7 @@ import org.srge.card.CardInfo;
 import org.srge.card.RunningInfo;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -20,15 +19,15 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MultiChoiceActivity extends CActionBarActivity {
-	//List<MultiInfo> quesList;
+	
 	ArrayList<CardInfo> cards = RunningInfo.getWorkingCardList();
+	//ArrayList<CardInfo> cards = RunningInfo.getSelectedDeck().getCardList();
 	int score = 0;
 	int qid = 0;
-
-	//MultiInfo currentQ;
+	
 
 	TextView txtQuestion;
-	RadioButton rda, rdb, rdc;
+	RadioButton rda, rdb, rdc, rdd;
 	Button butNext;
 
 	@Override
@@ -45,23 +44,31 @@ public class MultiChoiceActivity extends CActionBarActivity {
 		rda = (RadioButton) findViewById(R.id.radio_Multi0);
 		rdb = (RadioButton) findViewById(R.id.radio_Multi1);
 		rdc = (RadioButton) findViewById(R.id.radio_Multi2);
+		rdd = (RadioButton) findViewById(R.id.radio_Multi3);
 		butNext = (Button) findViewById(R.id.button_multi);
 		setQuestionView();
-
+		
 		butNext.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				RadioGroup grp = (RadioGroup) findViewById(R.id.radioMulti);
+				
+				//Get radio button answer
 				RadioButton answer = (RadioButton) findViewById(grp
 						.getCheckedRadioButtonId());
-				//Log.e("yourans", cards.get(qid).getAnswer() + " " + answer.getText());
-
+				
+				
+				//Update cards score and # of times seen
 				if (cards.get(qid).getAnswer().equals(answer.getText())) {
 					score++;
-					//Log.e("score", "Your score" + score);
+					cards.get(qid).answeredCorrect();
+				}
+				else{
+					cards.get(qid).answeredIncorrect();
 				}
 				
-				if (qid < (cards.size())) {
+				if ((qid != (cards.size()-1))) {
+					qid++;
 					setQuestionView();
 				} else {
 					 Intent intent = new Intent(MultiChoiceActivity.this,ResultActivity.class); Bundle b = new Bundle();
@@ -74,7 +81,7 @@ public class MultiChoiceActivity extends CActionBarActivity {
 		});
 		
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -83,11 +90,35 @@ public class MultiChoiceActivity extends CActionBarActivity {
 	}
 
 	private void setQuestionView() {
+		//Create array List with Answers
+		ArrayList<String> text =new ArrayList<String>(4);
+		
+		
+		/*
+		//Add Fake Answer
+		ArrayList<String> fake = cards.get(qid).getFakeAnswers();
+		for(String f:fake){
+			text.add(f);
+		}
+		*/
+		
+		//remove
+		for(int i = 0;i<3;i++){
+			text.add("fake answer"+i);
+		}
+		
+		text.add(cards.get(qid).getAnswer());
+		
+		//Randomize Radio Text Array
+		long seed = System.nanoTime();
+		Collections.shuffle(text, new Random(seed));
+		
+		//Set Text For Activity
 		txtQuestion.setText(cards.get(qid).getQuestion());
-		rda.setText("currentQ.getOPTA()"+qid);
-		rdb.setText("currentQ.getOPTB()"+qid);
-		rdc.setText("currentQ.getOPTC()"+score);
-		qid++;
+		rda.setText(text.get(0));
+		rdb.setText(text.get(1));
+		rdc.setText(text.get(2));
+		rdd.setText(text.get(3));		
 	}
 }
 
--- Bonsai/src/org/bonsai/activities/ResultActivity.java
+++ Bonsai/src/org/bonsai/activities/ResultActivity.java
@@ -1,8 +1,8 @@
 package org.bonsai.activities;
 
-
 import org.bonsai.util.CActionBarActivity;
 import org.srge.bonsai.R;
+import org.srge.card.RunningInfo;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,35 +10,42 @@ import android.view.Menu;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-public class ResultActivity extends  CActionBarActivity {
+public class ResultActivity extends CActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
-		//get rating bar object
-		RatingBar bar=(RatingBar)findViewById(R.id.ratingBar1); 
-		bar.setNumStars(5);
-		bar.setStepSize(0.5f);
-		//get text view
-		TextView t=(TextView)findViewById(R.id.textResult);
-		//get score
+		// get score object
+		TextView scoreText = (TextView) findViewById(R.id.text_score);
+
+		// get text view
+		TextView t = (TextView) findViewById(R.id.textResult);
+		// get score
 		Bundle b = getIntent().getExtras();
-		int score= b.getInt("score");
-		//display score
-		bar.setRating(score);
-		switch (score)
-		{
-		case 0:
-		case 1:
-		case 2: t.setText("Opps, try again bro, keep learning");
-		break;
-		case 3:
-		case 4:t.setText("Hmmmm.. maybe you have been reading a lot of quiz");
-		break;
-		case 5:t.setText("Who are you? A student in JP???");
-		break;
-		}
+		int score = b.getInt("score");
+
+		// display score
+		int deckSize = RunningInfo.getWorkingCardList().size();
+		scoreText.setText("Score: " + score + " out of " + deckSize);
+
+		// Score Based Text
+		int percentScore = (score / deckSize) * 100;
+		if (percentScore == 100)
+			t.setText("EXCELLENT 100%");
+		else if (percentScore >= 90)
+			t.setText("So close to perfection");
+		else if (percentScore >= 80)
+			t.setText("Good BUT not good enough");
+		else if (percentScore >= 70)
+			t.setText("Hmmmm.. maybe you have been reading a lot, but try to understand");
+		else if (percentScore >= 60)
+			t.setText("Opps, try again, keep learning");
+		else if (percentScore == 0)
+			t.setText("Kill Yourself: Your a Failure in LIFE");
+		else
+			t.setText("Try Again, Maybe this time open the book :(");
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
