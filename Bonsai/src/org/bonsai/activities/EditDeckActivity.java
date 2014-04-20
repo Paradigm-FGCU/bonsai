e8e47c93975c3f852616048a8b0019139551a543
Colin <yepitscolin@aim.com>
2014-04-20 11:29:55 -0400

    working on edit deck

--- Bonsai/src/org/bonsai/activities/EditDeckActivity.java
+++ Bonsai/src/org/bonsai/activities/EditDeckActivity.java
@@ -6,8 +6,11 @@ import java.util.Locale;
 import org.bonsai.activities.FlashActivity.SectionsPagerAdapter;
 import org.bonsai.util.CActionBarActivity;
 import org.srge.bonsai.R;
+import org.srge.card.CardInfo;
 import org.srge.card.RunningInfo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,18 +18,26 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
-public class EditDeckActivity extends CActionBarActivity{
+public class EditDeckActivity extends ActionBarActivity{
 	SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
-    
+    EditText deckNameEditText;
+    ArrayList<CardInfo> newList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,45 +47,100 @@ public class EditDeckActivity extends CActionBarActivity{
         android.app.ActionBar temp = getActionBar();
         temp.setTitle("Bonsai: Edit Deck");
         
-        
+        newList = (ArrayList<CardInfo>)RunningInfo.getSelectedDeck().getCardList().clone();
+        		
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the app.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
+        mSectionsPagerAdapter.activityParent = this;
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager_edit);
         mViewPager.setAdapter(mSectionsPagerAdapter);
         
-        EditText deckNameEditText = (EditText)findViewById(R.id.editable_deckname);
+        deckNameEditText = (EditText)findViewById(R.id.editable_deckname);
         deckNameEditText.setText(RunningInfo.getSelectedDeck().getDeckName());
         
+        final Button button_save_all = (Button) findViewById(R.id.edit_save_all);
+        button_save_all.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	AlertDialog.Builder builder = new AlertDialog.Builder(EditDeckActivity.this);
+            	builder
+            	.setTitle("Saving")
+            	.setMessage("Save All Changes?")
+            	.setIcon(android.R.drawable.ic_dialog_alert)
+            	.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            	    public void onClick(DialogInterface dialog, int which) {			      	
+            	    	updateDeck();
+            	    }
+            	})
+            	.setNegativeButton("No", null)						
+            	.show();
+            }
+        });
+        
     }
-
+    
+    public ArrayList<CardInfo> getDeck(){
+    	return this.newList;
+    }
+    
+    private void updateDeck(){
+    	RunningInfo.getSelectedDeck().setCardList(newList);
+    	//TODO update deck with database
+    	String readDeckName = deckNameEditText.getText().toString();
+    	if(readDeckName!=null) RunningInfo.getSelectedDeck().setDeckName(readDeckName);
+    	Toast.makeText(getApplicationContext(),
+				"Deck Pushed", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void updateCard(CardInfo temp,int index){
+    	newList.set(index,temp);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.card, menu);
+        getMenuInflater().inflate(R.menu.edit_action_bar_menu, menu);
         return true;
     }
-
     
+
     @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle presses on the action bar items
 	    switch (item.getItemId()) {
-	        case R.id.resources:
-	        	Intent intent = new Intent(EditDeckActivity.this,PeriodicTable.class);
-            	startActivityForResult(intent,0);
+	        case R.id.addCard:
+	        	AlertDialog.Builder builder = new AlertDialog.Builder(EditDeckActivity.this);
+            	builder
+            	.setTitle("Add Card")
+            	.setMessage("Are you sure?")
+            	.setIcon(android.R.drawable.ic_dialog_alert)
+            	.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            	    public void onClick(DialogInterface dialog, int which) {			      	
+            	    	addCard();
+            	    }
+            	})
+            	.setNegativeButton("No", null)						
+            	.show();
 	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
+    
+    public void addCard(){
+    	
+    	newList.add(new CardInfo(newList.get(newList.size()-1).getId()+1,"","",newList.get(0).getParentDeck()));
+    	mSectionsPagerAdapter.notifyDataSetChanged();
+    	mSectionsPagerAdapter.setPrimaryItem(mViewPager, newList.size()-1,mSectionsPagerAdapter.instantiateItem(mViewPager,newList.size()-1));
+    }
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    	EditDeckActivity activityParent;
     	
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -109,5 +175,7 @@ public class EditDeckActivity extends CActionBarActivity{
             return getString(R.string.title_section).toUpperCase(l)+" "+position;
 
         }
+        
+       
     }
 }
--- Bonsai/src/org/bonsai/activities/EditDeckFragment.java
+++ Bonsai/src/org/bonsai/activities/EditDeckFragment.java
@@ -1,16 +1,23 @@
 package org.bonsai.activities;
 
+import java.util.ArrayList;
+
 import org.bonsai.activities.FlashActivity.SectionsPagerAdapter;
 import org.bonsai.util.OnSwipeTouchListener;
 import org.srge.bonsai.R;
+import org.srge.card.CardInfo;
+import org.srge.card.DeckInfo;
 import org.srge.card.RunningInfo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -20,9 +27,11 @@ import android.widget.ViewSwitcher;
 
 public class EditDeckFragment extends Fragment{
 
-	private SectionsPagerAdapter parent;
-	private TextView definitionTextView; 
-	private TextView termTextView; 
+	private EditDeckActivity.SectionsPagerAdapter parent;
+	private TextView definitionEditText; 
+	private TextView termEditText; 
+	private TextView idTextView; 
+	private ArrayList<CardInfo> deck;
 
     public static final String ARG_SECTION_NUMBER = "section_number";
   
@@ -34,7 +43,7 @@ public class EditDeckFragment extends Fragment{
     @Override
 	public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	parent = RunningInfo.parent;
+    	parent = RunningInfo.parent2;
     	
     }
 
@@ -43,17 +52,42 @@ public class EditDeckFragment extends Fragment{
         Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_edit_card, container, false);
-
+        deck = parent.activityParent.getDeck();
         
-        EditText termEditText = (EditText)rootView.findViewById(R.id.editable_term);
-        EditText definitionEditText = (EditText)rootView.findViewById(R.id.editable_definition);
+        termEditText = (EditText)rootView.findViewById(R.id.editable_term);
+        definitionEditText = (EditText)rootView.findViewById(R.id.editable_definition);
+        idTextView = (TextView)rootView.findViewById(R.id.textViewID);
         
-
-        definitionEditText.setText(RunningInfo.getSelectedDeck().getCardList().get(getArguments().getInt(ARG_SECTION_NUMBER)-1).getQuestion());
-        termEditText.setText(RunningInfo.getSelectedDeck().getCardList().get(getArguments().getInt(ARG_SECTION_NUMBER)-1).getAnswer());
+        String term = deck.get(getArguments().getInt(ARG_SECTION_NUMBER)-1).getQuestion();
+        String definition = deck.get(getArguments().getInt(ARG_SECTION_NUMBER)-1).getAnswer();
+        if(term == null) term = "";
+        if(definition ==null) definition = "";
+        definitionEditText.setText(definition);
+        termEditText.setText(term);
     
-
-
+        final Button button_save_card = (Button) rootView.findViewById(R.id.button_save_card);
+        button_save_card.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	String term = termEditText.getText().toString();
+            	String definition = definitionEditText.getText().toString();
+            	if(term==null){
+        			Toast.makeText(getActivity().getApplicationContext(),
+            				"Error: term text on card", Toast.LENGTH_SHORT).show();
+        			return;
+        		}
+        		if(definition==null){
+        			Toast.makeText(getActivity().getApplicationContext(),
+            				"Error: definition text on card", Toast.LENGTH_SHORT).show();
+        			return;
+        		}
+        		CardInfo oldCard = deck.get(getArguments().getInt(ARG_SECTION_NUMBER)-1);
+        		CardInfo tempCard = new CardInfo(oldCard.getId(),definition,term,oldCard.getFakeAnswers(),oldCard.getParentDeck());
+            	parent.activityParent.updateCard(tempCard,getArguments().getInt(ARG_SECTION_NUMBER)-1);
+            	Toast.makeText(getActivity().getApplicationContext(),
+        				"Card Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
         
         return rootView;
     }
